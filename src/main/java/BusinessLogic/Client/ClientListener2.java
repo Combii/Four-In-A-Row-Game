6acf1c82a74584b6e6162ee0059b79a78a5dcbd5
Combii <davidstovle@gmail.com>
2017-04-05 17:42:10 +0200

    Deleted unused classes and .fxml files

--- src/main/java/BusinessLogic/Client/ClientListener2.java
+++ src/main/java/BusinessLogic/Client/ClientListener2.java
@@ -38,6 +38,10 @@ public class ClientListener2 implements Runnable{
         }
     }
 
+    public void checkProtocols(String message){
+
+    }
+
     public static void main(String[] args) throws IOException {
         new ClientListener2().run();
     }
